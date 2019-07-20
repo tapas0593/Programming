d11f0a9e78f6b73ff5d9f69f99f135394320d22f
admin <tptapaskumar9@gmail.com>
2019-07-20 10:20:29 +0530

    First Commit

---
+++ Functional/FlipCoin.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+public class FlipCoin {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of times you are flipping the coin");
+		int n=sc.nextInt();
+		int heads=0,tails=0,i=1;
+		if(n>0)
+		{
+		while(i<=n)
+		{
+			double value=Math.random();
+		if(value<0.5)
+		{
+			System.out.println(value+" Tails");
+			tails++;
+		}
+		else
+		{
+			System.out.println(value+" Heads");
+			heads++;
+		}
+		i++;
+		}		
+		System.out.println("Head Percentage vs Win Percentage");
+		System.out.println((double)heads*100/n+" vs "+(double)tails*100/n);
+		}
+		else
+		{
+			System.out.println("The number of flips should be a positive number");
+		}
+	}
+
+}
---
+++ Functional/HarmonicNumber.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class HarmonicNumber {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the value of N");
+		int n=sc.nextInt();
+		if(n==0)
+		System.out.println("Invalid input");
+		else
+		{
+			double h=nthHarmonic(n);
+			System.out.println("The nTh Harmonic value is="+h);
+		}
+	}
+	static double nthHarmonic(int n)
+	{
+		double sum=1;
+		for(int i=2;i<=n;i++)
+			sum=sum+(double)1/i;
+		return sum;
+	}
+
+}
